8682665f0479420e00e24abec18859c82fb432dd
freem11 <freem1985@gmail.com>
2023-07-27 16:26:37 -0600

    updated CTA buttons

--- src/components/AppleCTAButton.jsx
+++ src/components/AppleCTAButton.jsx
@@ -4,7 +4,7 @@ import applelogo from "../images/AppleCTA.png";
 
 export default function AppleCTAButton() {
   return (
-    <a href='https://apps.apple.com/us/app/divego/id6450968950'>
+    <a href='https://apps.apple.com/us/app/divego/id6450968950' target="_blank">
       <img src={applelogo} className="appleimg"></img>
     </a>
   );
--- src/components/GoogleCTAButton.jsx
+++ src/components/GoogleCTAButton.jsx
@@ -4,7 +4,7 @@ import googlelogo from "../images/GoogleCTA.png";
 
 export default function GoogleCTAButton() {
   return (
-    <a href='https://play.google.com/store/apps/details?id=com.freem11.divegomobile'>
+    <a href='https://play.google.com/store/apps/details?id=com.freem11.divegomobile' target="_blank">
       <img src={googlelogo} className="googleimg"></img>
     </a>
   );
