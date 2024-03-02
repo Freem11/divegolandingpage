132182684f602fd324c8c554c3fbc37e6ad45117
freem11 <freem1985@gmail.com>
2024-03-02 10:19:35 -0700

    social media logos and links added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Manta from "../images/Matt_Manta_White.png";
+import FacebookLogo from "../images/facebook.png";
+import InstagramLogo from "../images/instagram.png";
+import YoutubeLogo from "../images/youtube.png"
 import "./logo.css";
 
 function Logo() {
@@ -10,6 +13,18 @@ function Logo() {
         Scuba SEAsons
       </h2>
 
+      <a href='https://www.facebook.com/profile.php?id=61554622375177' target="_blank">
+      <img src={FacebookLogo} className="SMlogos"></img>
+    </a>
+
+    <a href='https://www.instagram.com/scuba_seasons/' target="_blank">
+    <img src={InstagramLogo} className="SMlogos1"></img>
+    </a>
+      
+    <a href='https://www.youtube.com/channel/UCAQzYoPicEgztcfT6r9xc6w' target="_blank">
+    <img src={YoutubeLogo} className="SMlogos2"></img>
+    </a>
+      
       <div className="buttonstyler" onClick={() => window.location = 'mailto:scubaseasons@gmail.com'}>
         Contact Us
       </div> 
@@ -18,3 +33,4 @@ function Logo() {
 }
 
 export default Logo;
+
